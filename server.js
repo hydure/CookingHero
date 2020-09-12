8c9f353753a957b1245b40e2292d6dc5c85329f8
Colin F. Lightfoot <cflightfoot@gmail.com>
2020-09-12 02:55:47 -0400

    Created backend APIs to add and fetch recipes

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
+const PORT = 4000;
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
-mongoose.set('useUnifiedTopology', true);
+const mongoose = require('mongoose').set('useUnifiedTopology', true);
+let Recipe = require('./src/Schemas/recipe.model');
 require('dotenv').config();
-const routes = express.Router();
-
-const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/recipes', routes);
 
 //Connect to mongo DB.
 console.log('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds013475.mlab.com:13475/recipes');
@@ -25,6 +23,34 @@ mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSW
         }
     });
 
+// Handling API Routes.
+const routes = express.Router();
+app.use('/recipes', routes);
+
+// Fetch all the saved recipes.
+routes.route('/').get(function(req, res) {
+    Recipe.find(function(err, recipes) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(recipes);
+        }
+    });
+});
+
+// Add a new recipe.
+routes.route('/add').post(function(req, res) {
+    let recipe = new Recipe(req.body);
+    recipe.save()
+        .then (recipe => {
+            res.status(200).json({'Recipe': "Recipe added successfully!"});
+        })
+        .catch (err => {
+            res.status(400).send("Adding a new recipe failed.");
+        });
+});
+
+// Listening for incoming connections on specified port.
 app.listen(PORT, function() {
     console.log("Server is running on port: " + PORT + ".");
 });
