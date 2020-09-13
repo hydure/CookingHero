de2c6b08800008c4ae65ba760dbc665b229ddda9
Samuel Lightfoot <salightfoot@comcast.net>
2020-09-13 08:41:15 -0400

    Rebuilt

---
+++ build/precache-manifest.9a5ccff254077c98bd6a3b06bf22a7bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a25033f1039f0a102ef8705818075b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f0eb8d258945ac76ec3",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3e4355cc67515bb68a0d",
+    "url": "/static/css/main.e4db8e93.chunk.css"
+  },
+  {
+    "revision": "2f0eb8d258945ac76ec3",
+    "url": "/static/js/2.ec1bf709.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ec1bf709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4355cc67515bb68a0d",
+    "url": "/static/js/main.ee908f66.chunk.js"
+  },
+  {
+    "revision": "1105d4a5f8277e63b185",
+    "url": "/static/js/runtime-main.69538387.js"
+  }
+]);
