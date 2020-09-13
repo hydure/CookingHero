6c4ac714f07cfc3ea46b86bc505b407d93ceb4a7
Colin F. Lightfoot <cflightfoot@gmail.com>
2020-09-12 21:41:01 -0400

    Pushing build folder

---
+++ build/precache-manifest.e33220796671da00b3f5cecbf1022838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de00a2153d7f507ea40c4b505da48fd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f0eb8d258945ac76ec3",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "96213af8bc0dbee668cb",
+    "url": "/static/css/main.7235c40a.chunk.css"
+  },
+  {
+    "revision": "2f0eb8d258945ac76ec3",
+    "url": "/static/js/2.ec1bf709.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ec1bf709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96213af8bc0dbee668cb",
+    "url": "/static/js/main.7424c6f6.chunk.js"
+  },
+  {
+    "revision": "1105d4a5f8277e63b185",
+    "url": "/static/js/runtime-main.69538387.js"
+  }
+]);
